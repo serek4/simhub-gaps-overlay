9b65dad12ef70103553b38367d62527d4731d9ca
serek4 <jacezarem@gmail.com>
2022-03-18 20:55:39 +0100

    added 2nd screen with sector deltas for +/- 1 pos
    simHub swoop plugin needed

--- gaps-overlay.js
+++ gaps-overlay.js
@@ -8,7 +8,7 @@ var textFont = "RussellSquare";
 var fontSize = 22;
 
 /**
- * check if there is driver ahead/behind 
+ * check if there is driver ahead/behind
  * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
  * @returns true or false
  */
@@ -16,7 +16,7 @@ function isDriver(offset) {
   return $prop(readDriverProp(offset, "Name")) ? true : false;
 }
 /**
- * 
+ *
  * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
  * @returns {number} driver overall position
  */
@@ -25,7 +25,7 @@ function driver_Position(offset) {
 }
 
 /**
- * 
+ *
  * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
  * @returns driver car number in #999 format
  */
@@ -34,7 +34,7 @@ function driver_CarNumber(offset) {
 }
 
 /**
- * 
+ *
  * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
  * @returns {string} driver name with short first name (J. Smith)
  */
@@ -46,7 +46,7 @@ function driver_Name(offset) {
 }
 
 /**
- * 
+ *
  * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
  * @returns {time} driver best lap time in mm.ss.fff format
  */
@@ -55,7 +55,7 @@ function driver_BestLapTime(offset) {
 }
 
 /**
- * 
+ *
  * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
  * @returns driver best lap delta to player
  */
@@ -81,7 +81,7 @@ function driver_HasBestLap(offset) {
 }
 
 /**
- * 
+ *
  * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
  * @returns {time} driver last lap time in mm.ss.fff format
  */
@@ -90,7 +90,7 @@ function driver_LastLapTime(offset) {
 }
 
 /**
- * 
+ *
  * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
  * @returns driver last lap delta to player
  */
@@ -101,7 +101,7 @@ function driver_LastLapDelta(offset) {
 }
 
 /**
- * 
+ *
  * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
  * @returns driver current gap to player (negative = ahead, positive = behind)
  */
@@ -110,7 +110,7 @@ function driver_Gap(offset) {
 }
 
 /**
- * 
+ *
  * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
  * @returns driver lap number relative to player
  */
@@ -119,8 +119,50 @@ function driver_RelativeLapNr(offset) {
   var playerLap = $prop("DataCorePlugin.GameData.CompletedLaps");
   return driverLap - playerLap;
 }
+
 /**
- * 
+ * get Swoop plugin data for ahead/behind or player car
+ * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
+ * @param {string} propName driver prop name to read
+ * @returns SimHub Swoop plugin prop
+ */
+function driver_SwoopProp(offset, propName) {
+  var relativePosition;
+  switch (offset) {
+    case -1:
+      relativePosition = "Ahead_";
+      break;
+    case 0:
+      relativePosition = "My";
+      break;
+    case 1:
+      relativePosition = "Behind_";
+      break;
+
+    default:
+      break;
+  }
+  return $prop("SimHubSwoopPlugin." + relativePosition + propName);
+}
+
+/**
+ * calculate opponent best sector time delta to player
+ * @param {number} offset driver position relative to player (negative = ahead, positive = behind)
+ * @param {number} sectorNr sector number to compare
+ * @returns delta in seconds
+ */
+function driver_SwoopBestSectorDelta(offset, sectorNr) {
+  var driverBestSector = timespantoseconds(
+    driver_SwoopProp(offset, "BestSector" + sectorNr)
+  );
+  var playerBestSector = timespantoseconds(
+    driver_SwoopProp(0, "BestSector" + sectorNr)
+  );
+  return driverBestSector === null ? null : playerBestSector - driverBestSector;
+}
+
+/**
+ *
  * @param {number} driverRelativePosition driver position relative to player (negative = ahead, positive = behind)
  * @param {string} propName driver prop name to read
  * @returns prop string name
